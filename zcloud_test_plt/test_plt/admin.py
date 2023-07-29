1b16dbee8812c20cb83466fe7454638d2d76de25
cuizhuofeng <727199799@qq.com>
2023-07-29 21:33:26 +0800

    增加注释；在admin注册model。

--- zcloud_test_plt/test_plt/admin.py
+++ zcloud_test_plt/test_plt/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import Project
 
-# Register your models here.
+# 将Models注册到这里
+admin.site.register(Project)
--- zcloud_test_plt/test_plt/models.py
+++ zcloud_test_plt/test_plt/models.py
@@ -1,6 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
 class Project(models.Model):
+    '''
+    测试项目的model
+    '''
     id = models.AutoField(primary_key=True)
+    #项目名称
     name = models.CharField(max_length=200)
