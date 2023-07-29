274092b3d03ab121b46f8f0c506e1bf5c178a281
cuizhuofeng <727199799@qq.com>
2023-07-29 22:21:26 +0800

    搞定显示

--- zcloud_test_plt/test_plt/apps.py
+++ zcloud_test_plt/test_plt/apps.py
@@ -2,4 +2,7 @@ from django.apps import AppConfig
 
 
 class TestPltConfig(AppConfig):
+    #面相程序的明显
     name = 'test_plt'
+    #前端显示
+    verbose_name="测试平台"
--- zcloud_test_plt/test_plt/models.py
+++ zcloud_test_plt/test_plt/models.py
@@ -7,5 +7,16 @@ class Project(models.Model):
     测试项目的model
     '''
     id = models.AutoField(primary_key=True)
-    #项目名称
-    name = models.CharField(max_length=200)
+    # 项目名称
+    name = models.CharField(max_length=200,verbose_name='测试项目名称')
+    # 默认显示
+    # 成功添加时返回添加的名字
+    def __str__(self):
+        return self.name
+
+    # 内部类Meta
+    class Meta:
+        # 对应右边页面操作页
+        verbose_name='测试项目'
+        # 对应左边栏目
+        verbose_name_plural=verbose_name
