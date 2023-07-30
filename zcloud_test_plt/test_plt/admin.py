469a6f77c0ff579ea4d1a9d2342783578178ada6
cuizhuofeng <727199799@qq.com>
2023-07-30 13:20:04 +0800

    增加ProjectAdmin类，对列表页面进行配置

--- zcloud_test_plt/test_plt/admin.py
+++ zcloud_test_plt/test_plt/admin.py
@@ -1,5 +1,18 @@
 from django.contrib import admin
 from .models import Project
+from django.contrib.admin import ModelAdmin
+
+
+class ProjectAdmin(ModelAdmin):
+    # 指定要显示的列（字段）
+    list_display = ['id', 'name', 'version', 'type', 'status', 'created_by', 'created_at']
+    # 指定哪些可进入项目链接
+    list_display_links = ['name']
+    # 指定可过滤的列
+    list_filter = ['created_at', 'type']
+    # 指定可查询的列
+    search_fields = ['name']
+
 
 # 将Models注册到这里
-admin.site.register(Project)
+admin.site.register(Project, ProjectAdmin)
